d3b8d63a6bee5d40d43294e142ef4e7a9c849d69
wj <2591290367@qq.com>
2020-03-04 21:43:22 +0800

    feat(全局)bg
    增加bg

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,21 +1,23 @@
 <template>
- <div class="page">
-   home
- </div>
+  <div class="homepage">home</div>
 </template>
 
 <script type="text/ecmascript-6">
 export default {
- data() {
- return {
-
- }
- },
- components: {
-
- }
-}
+  data() {
+    return {};
+  },
+  components: {}
+};
 </script>
 
 <style scoped lang="stylus">
+.homepage {
+  width: 100%;
+  height: 100%;
+  background: url('../assets/bg.jpg') no-repeat;
+  background-size: 100% 100%;
+  color: #fff;
+  font-size: 28px;
+}
 </style>
