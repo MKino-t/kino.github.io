9b085b46c8c554d469f9284379f3d7238e6c83f5
wj <2591290367@qq.com>
2020-03-06 15:41:35 +0800

    优化

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import Router from 'vue-router'
 Vue.use(Router)
 // console.log(Router)
 export default new Router({
-  mode: 'history',
+  // mode: 'history',
   routes: [{
     path: '/',
     component: () => import('@/views/index.vue'),
